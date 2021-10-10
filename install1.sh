0904f16d0a1ea09eacfe308f643853f6e1b778a2
NetFlex <84316160+Geruays@users.noreply.github.com>
2021-10-10 12:13:31 +0700

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -4,3 +4,4 @@ pkg install proot-distro
 
 proot-distro install debian
 
+proot-distro login debian -- echo -e "Default \e[32mGreen" && clear && echo this Script Made by NetFlex && apt-get update && apt-get install nodejs && npm i ngrok -g && clear && echo just run with ngrok && echo don't forget to paste ur authtoken && sleep 2 && clear && echo just run again with proot-distro login debian and write ngrok
